a76ec6a242b2b464447e129ecbc6a34879b9846a
Kevin Day <34363465+kevin-ch-day@users.noreply.github.com>
2025-08-18 21:03:50 -0500

    Expose sandbox APIs and fix process listing error

--- sandbox/__init__.py
+++ sandbox/__init__.py
@@ -1,5 +1,15 @@
 """Sandbox runtime analysis utilities."""
 
 from .metrics import compute_runtime_metrics
+from .sandbox_runner import run_sandbox
+from .permission_monitor import collect_permissions
+from .network_sniffer import sniff_network
+from .analysis import analyze_apk
 
-__all__ = ["compute_runtime_metrics"]
+__all__ = [
+    "compute_runtime_metrics",
+    "run_sandbox",
+    "collect_permissions",
+    "sniff_network",
+    "analyze_apk",
+]
